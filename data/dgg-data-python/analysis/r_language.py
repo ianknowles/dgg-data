cf093882b47aaed54bfcb6acf048df7d6284ce69
ianknowles <ian@imknowles.co.uk>
2022-03-10 10:40:29 +0000

    improved log messages for the R wrapper

--- data/dgg-data-python/analysis/r_language.py
+++ data/dgg-data-python/analysis/r_language.py
@@ -13,13 +13,13 @@ class RExecutable:
 		# TODO find the install path, or just place in config file?
 		self.filepath = 'C:\\Program Files\\R\\R-3.5.0\\bin\\Rscript.exe'
 		if not os.path.isfile(self.filepath):
-			logger.error('Cannot find R executable')
+			logger.error(f'Cannot find R executable {self.filepath}')
 			raise FileNotFoundError
 
 	def run_script(self, script_filepath):
 		"""Run the given R script with the R executable and log its output"""
 		try:
-			logger.info('Running R script {file}'.format(file=os.path.basename(script_filepath)))
+			logger.info(f'Running R script {os.path.basename(script_filepath)}')
 			# TODO RECHECK
 			p = subprocess.Popen([self.filepath, os.path.basename(script_filepath)], cwd=os.path.dirname(script_filepath), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 			with p.stdout:
