657356426d96fc133191c1ff3b1863274860efe1
ianknowles <ian@imknowles.co.uk>
2022-03-11 08:33:15 +0000

    move redo functions to separate file

--- data/dgg-data-python/analysis/r_analysis_wrapper.py
+++ data/dgg-data-python/analysis/r_analysis_wrapper.py
@@ -1,6 +1,5 @@
 """Methods for running the R analysis script. Run this script to analyse today's collection"""
 import datetime
-import json
 import os
 
 from analysis import r_language
@@ -64,53 +63,6 @@ def predict(batch_string, estimate='mau'):
 		s3_bucket.put(key, countfile)
 
 	return key
-
-
-def catchup_analysis():
-	"""Run an analysis for any collections that haven't been analysed yet"""
-	import boto3
-
-	with open(s3_auth) as key_file:
-		s3_keys = json.load(key_file)
-		client = boto3.client('s3', **s3_keys)
-
-	paginator = client.get_paginator('list_objects')
-	result = paginator.paginate(Bucket='www.digitalgendergaps.org', Prefix='data/', Delimiter='/')
-	for prefix in result.search('CommonPrefixes'):
-		batch_string = prefix['Prefix'].split('/')[1]
-		try:
-			response = client.get_object(Bucket='www.digitalgendergaps.org', Key=prefix['Prefix'] + 'monthly_model.csv')
-		except client.exceptions.NoSuchKey:
-			try:
-				# TODO exceptions not for control flow, may be limited by boto3 available methods
-				predict(batch_string)
-			except Exception as e:
-				logger.error('Exception in batch {x}, {e}'.format(x=batch_string, e=e))
-
-
-def redo_analysis():
-	"""Run a new analysis for all datasets in the bucket"""
-	import boto3
-
-	with open(s3_auth) as key_file:
-		s3_keys = json.load(key_file)
-		client = boto3.client('s3', **s3_keys)
-
-	from analysis.analysis_index import ModelIndexFile
-	s3_bucket = S3Bucket()
-	index = ModelIndexFile(s3_bucket, 'data/models2.json')
-
-	paginator = client.get_paginator('list_objects')
-	result = paginator.paginate(Bucket='www.digitalgendergaps.org', Prefix='data/', Delimiter='/')
-	for prefix in result.search('CommonPrefixes'):
-		batch_string = prefix['Prefix'].split('/')[1]
-		try:
-			mau_key = predict(batch_string)
-
-			index.add_entry(batch_string, mau_key)
-			predict(batch_string, 'dau')
-		except Exception as e:
-			logger.error('Exception in batch {x}, {e}'.format(x=batch_string, e=e))
 
 
 if __name__ == "__main__":
---
+++ data/dgg-data-python/analysis/redo.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+from analysis.r_analysis_wrapper import predict
+from dgg_log import root_logger
+from storage.S3_bucket import S3Bucket
+from storage.dgg_file_structure import auth_path
+
+logger = root_logger.getChild(__name__)
+
+s3_auth = os.path.join(auth_path, 'S3_keys.json')
+
+
+def catchup_analysis():
+	"""Run an analysis for any collections that haven't been analysed yet"""
+	import boto3
+
+	with open(s3_auth) as key_file:
+		s3_keys = json.load(key_file)
+		client = boto3.client('s3', **s3_keys)
+
+	paginator = client.get_paginator('list_objects')
+	result = paginator.paginate(Bucket='www.digitalgendergaps.org', Prefix='data/', Delimiter='/')
+	for prefix in result.search('CommonPrefixes'):
+		batch_string = prefix['Prefix'].split('/')[1]
+		try:
+			response = client.get_object(Bucket='www.digitalgendergaps.org', Key=prefix['Prefix'] + 'monthly_model.csv')
+		except client.exceptions.NoSuchKey:
+			try:
+				# TODO exceptions not for control flow, may be limited by boto3 available methods
+				predict(batch_string)
+			except Exception as e:
+				logger.error('Exception in batch {x}, {e}'.format(x=batch_string, e=e))
+
+
+def redo_analysis():
+	"""Run a new analysis for all datasets in the bucket"""
+	import boto3
+
+	with open(s3_auth) as key_file:
+		s3_keys = json.load(key_file)
+		client = boto3.client('s3', **s3_keys)
+
+	from analysis.analysis_index import ModelIndexFile
+	s3_bucket = S3Bucket()
+	index = ModelIndexFile(s3_bucket, 'data/models2.json')
+
+	paginator = client.get_paginator('list_objects')
+	result = paginator.paginate(Bucket='www.digitalgendergaps.org', Prefix='data/', Delimiter='/')
+	for prefix in result.search('CommonPrefixes'):
+		batch_string = prefix['Prefix'].split('/')[1]
+		try:
+			mau_key = predict(batch_string)
+
+			index.add_entry(batch_string, mau_key)
+			predict(batch_string, 'dau')
+		except Exception as e:
+			logger.error('Exception in batch {x}, {e}'.format(x=batch_string, e=e))
