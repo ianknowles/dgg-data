a0290b2331b35114dfaef2272215daf18aa92300
ianknowles <ian@imknowles.co.uk>
2022-03-11 09:46:43 +0000

    move part of the predict function to a separate function, predicting from a local file can be chained into different tasks where preprocessing and uploads are not needed

--- data/dgg-data-python/analysis/r_analysis_wrapper.py
+++ data/dgg-data-python/analysis/r_analysis_wrapper.py
@@ -44,28 +44,34 @@ def cleanup_r_script_outputs(output_path):
 		pass
 
 
+def predict_from_file(script_filepath, input_filepath, output_path):
+	cleanup_r_script_outputs(output_path)
+
+	logger.info('Beginning analysis')
+	r_exe = r_language.RExecutable()
+	r_exe.run_script(os.path.join(script_filepath, "Digital_gender_gaps_analysis_updated_7Nov2019.R"), [input_filepath, output_path])
+	logger.info('Analysis complete')
+
+	return {'predictions': os.path.join(output_path, 'Appendix_table_model_predictions.csv'), 'fits': os.path.join(output_path, 'fits.csv')}
+
+
 def predict(batch_string, estimate='mau'):
 	"""Run an analysis for the given day. Inputs are expected to be retrievable from S3"""
 	preprocess_analysis_data(batch_string, estimate)
 
 	s3_bucket = S3Bucket()
 
-	cleanup_r_script_outputs(data_path)
-
-	logger.info('Beginning analysis')
-	r_exe = r_language.RExecutable()
-	r_exe.run_script(os.path.join(r_path, "Digital_gender_gaps_analysis_updated_7Nov2019.R"), [f'{estimate}_counts_{batch_string}.csv'])
-	logger.info('Analysis complete')
+	files = predict_from_file(r_path, f'{estimate}_counts_{batch_string}.csv', data_path)
 
 	batch_s3_folder = f'data/{batch_string}'
 	key = ''
-	with open(os.path.join(data_path, 'Appendix_table_model_predictions.csv'), 'rb') as file:
+	with open(files['predictions'], 'rb') as file:
 		filename = f'{estimate}_monthly_model_2_{batch_string}.csv'
 		key = f'{batch_s3_folder}/{filename}'
 		s3_bucket.put(key, file)
 		logger.info(f'Uploaded {filename}')
 
-	with open(os.path.join(data_path, 'fits.csv'), 'rb') as file:
+	with open(files['fits'], 'rb') as file:
 		filename = f'{estimate}_monthly_model_2_{batch_string}_fits.csv'
 		fit_key = f'{batch_s3_folder}/{filename}'
 		s3_bucket.put(fit_key, file)
