d3227c1ee50139bf320f8230fb623f0268788284
ianknowles <ian@imknowles.co.uk>
2022-03-11 11:14:04 +0000

    add path file for module folder structure

---
+++ analysis/source/paths.py
@@ -0,0 +1,11 @@
+"""Module path definitions setting out the local folder structure"""
+import os
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+module_path = os.path.normpath(os.path.join(file_path, '..'))
+
+log_path = os.path.join(module_path, 'logs')
+input_path = os.path.join(module_path, 'input')
+count_path = os.path.join(input_path, 'counts')
+output_path = os.path.join(module_path, 'output')
+r_path = os.path.join(module_path, 'source')
